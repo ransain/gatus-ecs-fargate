bf2dbfd558c14978692757bc04aafe8459f94db4
imran <imran.husain@hotmail.com>
2026-07-04 00:29:38 +0100

    added cpu and memory value to task definition

--- infra/modules/ecs/main.tf
+++ infra/modules/ecs/main.tf
@@ -7,15 +7,17 @@ resource "aws_ecs_cluster" "gatus_ecs" {
   }
 }
 
-resource "aws_ecs_task_definition" "service" {
+resource "aws_ecs_task_definition" "gatus_task_def" {
   family                   = "gatus-td"
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
+  cpu = "256"
+  memory = "512g"
   container_definitions = jsonencode([
     {
       name      = "gatus-app"
       image     = var.container_image
-      cpu       = 10
+      cpu       = 256
       memory    = 512
       essential = true
       portMappings = [
